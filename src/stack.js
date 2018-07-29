5fdd0677370d0bc3e87a432a633bd97068a328c9
Alexey Berezin <beraliv.spb@gmail.com>
2018-07-29 11:50:28 +0300

    prod: add stack

---
+++ src/stack.js
@@ -0,0 +1,26 @@
+let StackFactory = () => {
+    let elements;
+    class Stack {
+        constructor() {
+            elements = []
+        }
+
+        push(element) {
+            elements.push(element)
+        }
+
+        pop() {
+            let element = elements[elements.length - 1]
+            elements = elements.slice(0, -1)
+            return element
+        }
+
+        get top() {
+            return elements[elements.length - 1]
+        }
+    }
+
+    return new Stack()
+}
+
+export default StackFactory
